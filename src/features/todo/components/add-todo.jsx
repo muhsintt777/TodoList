469b7f08c606db7331a9e29b1377ee1484eb9524
Muhsin <muhsintt777@gmail.com>
2024-06-09 15:41:29 +0530

    feat : add add todo

--- src/features/todo/components/add-todo.jsx
+++ src/features/todo/components/add-todo.jsx
@@ -1,14 +1,20 @@
+import { addTodo } from "services/todoService";
 import styles from "./add-todo.module.css";
+import { useDispatch } from "react-redux";
+import { fetchTodos } from "../todoSlice";
 
 export const AddTodo = () => {
+  const dispatch = useDispatch();
+
+  async function handleTodoSubmit(e) {
+    e.preventDefault();
+    await addTodo(e.target.todo.value);
+    dispatch(fetchTodos());
+  }
+
   return (
-    <form className={styles.form}>
-      <input
-        className={styles.headerInput}
-        required
-        // onChange={}
-        type="text"
-      />
+    <form className={styles.form} onSubmit={handleTodoSubmit}>
+      <input className={styles.headerInput} required type="text" name="todo" />
       <button className={styles.headerButton}>Add</button>
     </form>
   );
