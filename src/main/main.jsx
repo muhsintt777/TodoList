4212a04ca6327b36742fa84331d37e667d6dba64
Muhsin <muhsintt777@gmail.com>
2024-06-09 14:38:53 +0530

    fix : store import

--- src/main/main.jsx
+++ src/main/main.jsx
@@ -2,7 +2,7 @@ import "./index.css";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
-import { store } from "./app/store";
+import { store } from "configs/store";
 import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
